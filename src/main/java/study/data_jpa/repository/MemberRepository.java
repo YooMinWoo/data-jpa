1695e42c99a8d47c05a7e7cda5eba99c1451be8c
YooMinWoo <alsn99@codeline.kr>
2025-02-28 17:01:42 +0900

    setction5_순수 JPA 페이징과 정렬

---
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package study.data_jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import study.data_jpa.dto.MemberDto;
+import study.data_jpa.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.data_jpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username);   // 컬렉션
+    Member findMemberByUsername(String username);   // 단건
+    Optional<Member> findOptionalByUsername(String username);   //단건 Optional
+
+}
